8d16856dc38c20622dc1719f522ea7e8c5e44139
deepthijv <deepthijagalur@gmail.com>
2023-12-06 02:33:49 -0500

    Adding Flask Code

--- myapp/models.py
+++ myapp/models.py
@@ -2,4 +2,7 @@ from .extensions import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
+    firstname = db.Column(db.String(50))
+    lastname=db.Column(db.String(50))
+    phone=db.Column(db.Integer(20))
+    email=db.Column(db.String(50))
--- myapp/routes.py
+++ myapp/routes.py
@@ -4,33 +4,37 @@ from .models import User
 
 main = Blueprint('main', __name__)
 
-@main.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
+@main.route('/users/<email>', methods=['GET'])
+def get_users(email):
+    user = User.query.filter_by(email=email).first()
 
-    if users:
+    if user:
         # Return the first user directly
-        user = users[0]
-        user_dict = {'id': user.id, 'username': user.username}
+        user_dict = {'id': user.id, 'firstname': user.firstname,'lastname':user.lastname,'phone':user.phone,'email':user.email}
         return jsonify(user_dict)
 
-@main.route('/', methods=['POST'])
+@main.route('/adduser/', methods=['POST'])
 def add_user():
-    data = request.get_json()
+        data = request.get_json()
 
-    if 'username' not in data:
-        return jsonify({'error': 'Missing username parameter'}), 400
+        if 'username' not in data:
+            return jsonify({'error': 'Missing username parameter'}), 400
 
-    new_user = User(username=data['username'])
-    db.session.add(new_user)
-    db.session.commit()
+        # Extract additional user information from the request
+        firstname = data.get('firstname', '')
+        lastname = data.get('lastname', '')
+        phone = data.get('phone', '')
+        email = data.get('email', '')
 
-    return jsonify({'message': 'User added successfully'}), 201
+        # Create a new user with the provided information
+        new_user = User(
+            username=data['username'],
+            firstname=firstname,
+            lastname=lastname,
+            phone=phone,
+            email=email
+        )
 
-# Additional route to add a user using URL parameter
-@main.route('/add/<username>', methods=['GET'])
-def add_user_from_url(username):
-    new_user = User(username=username)
-    db.session.add(new_user)
-    db.session.commit()
-    return redirect(url_for("main.get_users"))
+        # Add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
