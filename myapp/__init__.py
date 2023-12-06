cc8377d941309a25ab574f398414bd7de3976954
deepthijv <deepthijagalur@gmail.com>
2023-12-06 10:11:14 -0500

    Adding Flask Code

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -8,7 +8,7 @@ from .routes import main
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://smartparkuserdb_user:q5WDQuPmGjrLGJNVbQuC7Q1j9eQTBM9a@dpg-clnkokgapebc739qjbng-a.oregon-postgres.render.com/smartparkuserdb"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://smartpark_user_db_user:93k5SkAxyRqiZ7l6hHxXydPrFSLpMIrn@dpg-clo2h0n5felc73a1ca5g-a/smartpark_user_db"
     # postgres://prettyprinted_render_example_user:11vq6k72GmFJazhVpz3pFUko50djVZT1@dpg-ceukdhmn6mpglqdb4avg-a.oregon-postgres.render.com/prettyprinted_render_example
 
     db.init_app(app)
